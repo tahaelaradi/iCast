ec5393ad1259cc1327e174cc32e98d7d35017e03
Taha Elaradi <ta.elaradi23@gmail.com>
2019-05-30 23:57:34 -0400

    Removed unneeded CSS

--- ClientApp/src/components/PodcastCard/PodcastCard.jsx
+++ ClientApp/src/components/PodcastCard/PodcastCard.jsx
@@ -25,26 +25,25 @@ class PaginationBar extends Component {
     return (
       <Card className="shadow-sm m-2 bg-white rounded">
         <CardBody>
-          <CardTitle className="text-uppercase p-2 mb-2">
+          <CardTitle className="text-uppercase p-2 mb-2" >
+            <a
+              className="shadow-none text-center"
+              href={"player/" + this.props.item.id}
+            >
             {this.props.item.title}
+            </a>
           </CardTitle>
           <CardSubtitle className="font-italic">
             {this.props.item.author}
           </CardSubtitle>
           <Row className="my-2">
             <Col sm="4">
-              <CardImg top width="100%" src={imgsrc} alt="Card image cap" />
+              <CardImg top width="100%" src={imgsrc} alt="Card image cap" href={"player/" + this.props.item.id}/>
             </Col>
             <Col sm="8">
               <CardText className="">{description}</CardText>
             </Col>
           </Row>
-          <a
-            className="shadow-none text-center"
-            href={"player/" + this.props.item.id}
-          >
-            Go somewhere
-          </a>
         </CardBody>
       </Card>
     );
